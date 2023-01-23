66fcbfccc022c90749a719eb402233540e63e729
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-23 19:12:51 -0300

    termina o quinto exercício

--- _fundamentos/secao-3-introducao-javascript/scripts2.js
+++ _fundamentos/secao-3-introducao-javascript/scripts2.js
@@ -37,9 +37,9 @@ let number1 = 2;
 let number2 = 2;
 let subtracao = number2 - number1;
 
-if (subtracao > 0 ) {
+if (subtracao > 0) {
     console.log('positivo');
-} else if (subtracao < 0){
+} else if (subtracao < 0) {
     console.log('negativo');
 } else {
     console.log('zero');
@@ -55,10 +55,51 @@ let angle3 = 60;
 
 let angleSum = angle1 + angle2 + angle3;
 
-if (angleSum === 180){
+if (angleSum === 180) {
     console.log(true, 'É um triângulo!')
 } else if (angleSum > 180) {
     console.log(false, 'Não é um triângulo')
 } else {
     console.log('Erro, não é um polígono')
+};
+
+// Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let pecaXadrez = 'peão';
+let modifiedName = pecaXadrez.toUpperCase();
+
+
+
+    modifiedName = modifiedName.replace(/[ÀÁÂÃÄÅ]/,"A");
+    modifiedName = modifiedName.replace(/[àáâãäå]/,"A");
+    modifiedName = modifiedName.replace(/[ÈÉÊË]/,"E");
+
+
+switch (modifiedName) {
+    case 'BISPO':
+        console.log('Diagonal')
+    break
+    
+    case 'REI':
+        console.log('Pode mover-se em qualquer direção, porém apenas uma casa por vez, como indicam os pontos vermelhos na figura abaixo.')
+    break
+
+    case 'DAMA':
+        console.log('Assim como o Rei, pode mover-se em qualquer direção -vertical, horizontal e diagonal-, porém quantas casas quiser, desde que estejam livres.')
+    break
+
+    case 'TORRE':
+        console.log('Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.')
+    break
+
+    case 'CAVALO':
+        console.log('É a única peça que pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas. O movimento executado pelo Cavalo é conhecido por “um-dois” ou “em L”. Ele pode andar duas casas na horizontal e uma na vertical, ou duas na vertical e uma na horizontal, uma na horizontal e duas na vertical, e assim por diante. Quando a casa de saída do Cavalo for escura, a casa de chegada será clara, e vice-versa.')
+    break 
+
+    case 'PEAO':
+        console.log('Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas. O peão é a única peça que não pode retroceder, e também a única que efetua a captura com um movimento diferente do utilizado para avançar no tabuleiro. O peão pode capturar as peças que estejam uma fileira acima, mas nas colunas adjacentes a sua.')
+    break    
+
+    default:
+        console.log('Peça inválida')
 }
