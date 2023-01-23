db6d293ab7fd595e7a6ea26b8756b8e568596039
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-23 18:51:25 -0300

    termina o quarto exercicio

--- _fundamentos/secao-3-introducao-javascript/scripts2.js
+++ _fundamentos/secao-3-introducao-javascript/scripts2.js
@@ -45,4 +45,20 @@ if (subtracao > 0 ) {
     console.log('zero');
 }
 
+// Utilize if/else para escrever um código que defina três constantes com os valores dos três ângulos internos de um triângulo.
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, 
+// você deve retornar uma mensagem de erro.
 
+let angle1 = 60;
+let angle2 = 60;
+let angle3 = 60;
+
+let angleSum = angle1 + angle2 + angle3;
+
+if (angleSum === 180){
+    console.log(true, 'É um triângulo!')
+} else if (angleSum > 180) {
+    console.log(false, 'Não é um triângulo')
+} else {
+    console.log('Erro, não é um polígono')
+}
