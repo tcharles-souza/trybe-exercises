d9faff0f07d134247488950dd77084b36466b992
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-23 16:17:20 -0300

    faz o exercicio 1

---
+++ _fundamentos/secao-3-introducao-javascript/scripts.js
@@ -0,0 +1,13 @@
+let a = 3;
+let b = 17;
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+
+console.log('a = ', a);
+console.log('b = ', b);
+console.log('a + b = ', adicao);
+console.log('a - b = ', subtracao);
+console.log('a * b = ', multiplicacao);
+console.log('a / b = ', divisao);
