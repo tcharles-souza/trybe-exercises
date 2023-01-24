c9542de5c85e72a35b72893ff5b35f49d4eb1067
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-24 10:33:16 -0300

    termina o terceiro exercicio

--- _fundamentos/secao-3-introducao-javascript/script3.js
+++ _fundamentos/secao-3-introducao-javascript/script3.js
@@ -22,3 +22,23 @@ if (number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0) {
     isOdd = true;
 }
 console.log(isOdd);
+
+//      Utilize if/else para escrever um código que se inicie com dois valores em duas constantes diferentes: o custo de 
+// um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda, descontado o custo do 
+// produto) a empresa terá ao vender mil desses produtos.
+
+const purchaseValue = 150.00;
+
+// Custo total = valor de compra + 20% de imposto
+
+const totalCost= purchaseValue * 1.2;
+const saleValue = 300.00; 
+let profit;
+
+if (purchaseValue < 0 || saleValue < 0){
+    console.log('Erros, valores incorretos.');
+} else {
+    profit = 1000 * (saleValue - totalCost);
+}
+console.log()
+console.log(`Lucro total R$ ${profit.toFixed(2)}`)
