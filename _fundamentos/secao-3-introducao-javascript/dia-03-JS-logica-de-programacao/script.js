017c3515e9c0495b4affd47deb0778dabf54ab23
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-25 19:28:33 -0300

    Cria o exercicio

---
+++ _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/script.js
@@ -0,0 +1,10 @@
+// Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+let produtoFatorial = 1;
+let fatorial = 10;
+
+for (let i = fatorial; i > 0; i--){
+    produtoFatorial = produtoFatorial*i
+}
+
+console.log(produtoFatorial)
