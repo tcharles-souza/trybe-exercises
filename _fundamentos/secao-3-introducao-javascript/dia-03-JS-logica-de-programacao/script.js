c454b2a297c470cd6a72f0c6c2a6c4dd79bddd62
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-25 19:39:08 -0300

    termina o segundo quesito dos exercicios

--- _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/script.js
+++ _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/script.js
@@ -8,3 +8,17 @@ for (let i = fatorial; i > 0; i--){
 }
 
 console.log(produtoFatorial)
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo,
+// a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para 
+// verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let invertedWord = '';
+
+for(i = word.length-1; i >= 0; i--){
+    invertedWord = invertedWord + word[i]
+}
+
+console.log(invertedWord)
+
