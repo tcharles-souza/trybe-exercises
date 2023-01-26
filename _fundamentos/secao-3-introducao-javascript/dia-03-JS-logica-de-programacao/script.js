cb5dad97fac1f6e63cbb234083a4043685629942
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-25 23:08:54 -0300

    Termina o exercício

--- _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/script.js
+++ _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/script.js
@@ -58,13 +58,51 @@ console.log('Menor palavra: ', menorPalavra)
 //  Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele 
 //  mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50
 
-let primo = false;
 
-for (let i = 3; i < 50; i++){
-    for (let j = 2; j < i; j++){
-        if(i%j == 0){
-        primo = false;
-    } else {
+// Crio duas variáveis, uma vai guardar o intervalo de números, a outra irei separar os números primos.
+
+let numbers = [];
+let primos = [];
+
+//Cria o intervalo de números:
+
+for (let i = 2; i <= 50; i++){
+    numbers.push(i);
+}
+
+//Aqui faço a separação: se não é primo, então é false. Para isso:
+// 1 - faço uma varredura no meu intervalo de número e analiso cada um
+// 2 - Análise: pego um número "numbers[i]" e divido esse número por 2 até "numbers[i]-1"
+// por exemplo: o número 7 é divido por (2, 3, 4, 5, 6). Como nenhuma dessas divisões deixa resto 0, o valor de "numbers[i]"
+// não é alterado para false.
+// 3- Agora todos os números do meu array que não são primos foram atribuidos como false.
+
+for (let i = 0; i < numbers.length; i++){
+    for (j = 2; j < numbers[i] ; j++){
+        if (numbers[i]%j === 0){
+            numbers[i] = false;
+        }
     }
 }
+// Para fins de visualização, retiro todos os "false" do meu array e crio uma variável contendo somente os números primos:
+
+for (let value of numbers){
+    if (value !== false){
+        primos.push(value)
+    }
 }
+
+console.log('números primos:', primos)
+
+//  Crio um váriavel de valor 0; faço uma varredura para saber qual "maiorPrimo[i]" é maior que a variável e atribuo a esta
+//o valor, caso ele seja maior:
+
+let maiorPrimo = 0;
+
+for(let value of primos){
+    if (value > maiorPrimo){
+        maiorPrimo = value;
+    }
+}
+console.log(primos)
+console.log(`Maior primo encontrado no intervalo = ${maiorPrimo}`)
