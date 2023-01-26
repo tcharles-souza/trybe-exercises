7557a60d5e38d62e29529c1e5f90749818d4806b
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-26 16:35:18 -0300

    faz um códiog, por acidente, que gera um seta de asterísticos

--- _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/bonus.js
+++ _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/bonus.js
@@ -87,27 +87,31 @@ console.log('Exercício IV -------------------------------------------')
 console.log()
 
 
-// Depois, faça uma pirâmide com n asteriscos de base:
+// Depois, faça uma pirâmide com n asteriscos de base n
+
 //ideia = qual sequencia ira passar?
 
-// 0 1 "2" 3 4   -> seleciona 1 numero            n
-// 0"1 2 3" 4    -> seleciona 3 numeros       -1n n +1n 
-//"0 1 2 3 4"    -> seleciona 5 numeros   -1n -1n n +1n +1n ou seja, +2 n
+// 0 1 "2" 3 4   -> seleciona 1 numero            n                             i = 0   middle =  2 
+// 0"1 2 3" 4    -> seleciona 3 numeros       -1n n +1n                         i = 1   middle =  123
+//"0 1 2 3 4"    -> seleciona 5 numeros   -1n -1n n +1n +1n ou seja, +2 n       i = 2   middle =  1234
 //               -> seleciona 7 numeros ...
 
-n = 5;
+n = 9;
 let triangulo = ''
 let middleNumber = Math.floor(n/2)
+let test = -1;
 
-for (let i = 0; i < n; i++) {
-    if (i == middleNumber) {
-        triangulo = triangulo + '*'
-    } else {
-        triangulo = triangulo + ' '
+for (let i = 0; i < n; i += 1){
+    test++
+    triangulo = ''
+
+    for (let i = 0; i < n; i += 1) {
+        if (i == middleNumber || i == (middleNumber - test) || i == (middleNumber + test)) {
+            triangulo = triangulo + '*'
+        } else {
+            triangulo = triangulo + ' '
+        }
     }
-    
     console.log(triangulo)
 }
-
-
 
---
+++ _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/seta.js
@@ -0,0 +1,30 @@
+// ---->** sem querer fiz uma seta! Ficou massa!
+
+
+// Depois, faça uma pirâmide com n asteriscos de base n
+
+//ideia = qual sequencia ira passar?
+
+// 0 1 "2" 3 4   -> seleciona 1 numero            n                             i = 0   middle =  2 
+// 0"1 2 3" 4    -> seleciona 3 numeros       -1n n +1n                         i = 1   middle =  123
+//"0 1 2 3 4"    -> seleciona 5 numeros   -1n -1n n +1n +1n ou seja, +2 n       i = 2   middle =  1234
+//               -> seleciona 7 numeros ...
+
+n = 97;
+let triangulo = ''
+let middleNumber = Math.floor(n/2)
+let test = -1;
+
+for (let i = 0; i < n; i += 1){
+    test++
+    triangulo = ''
+
+    for (let i = 0; i < n; i += 1) {
+        if (i == middleNumber || i == (middleNumber - test) || i == (middleNumber + test)) {
+            triangulo = triangulo + '*'
+        } else {
+            triangulo = triangulo + ' '
+        }
+    }
+    console.log(triangulo)
+}
