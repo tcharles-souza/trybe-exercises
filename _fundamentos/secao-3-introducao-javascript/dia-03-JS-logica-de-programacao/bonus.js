f398d50ba41b0709bb16ffc30a70875b2e482d4d
Tcharles Souza <tcharlesvicente@gmail.com>
2023-01-25 23:37:58 -0300

    termina o exercício bônus 1

---
+++ _fundamentos/secao-3-introducao-javascript/dia-03-JS-logica-de-programacao/bonus.js
@@ -0,0 +1,34 @@
+// Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de
+// lado de tamanho n. Por exemplo:
+
+console.log()
+console.log('Exercício I -------------------------------------------')
+console.log()
+
+// Escolhe um número aleatŕio de 1 a 20 e guarda na variável n
+
+
+let n = (Math.floor(Math.random() * 19) + 2)
+let quadrado = '';
+
+//Cria um váriavel 'quadrado' que irá conter a quantidade de 'n' asterísticos
+
+
+for (let i = 0; i < n; i++) {
+    quadrado = quadrado + '*'
+}
+
+console.log('Quadrado de lado', n)
+
+// O contador irá contar de 0 até n-1 e irá imprimir na tela a variável quadrada n vezes:
+
+let count = 0;
+
+while (count < n) {
+    count++
+    console.log(quadrado)
+}
+
+console.log()
+console.log('Exercício II -------------------------------------------')
+console.log()
