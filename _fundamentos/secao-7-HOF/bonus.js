9753616161a1f7b68cb70d3550c609f14318d1f2
Tcharles Souza <tcharlesvicente@gmail.com>
2023-03-06 14:15:58 -0300

    Termina os exercícios

--- _fundamentos/secao-7-HOF/bonus.js
+++ _fundamentos/secao-7-HOF/bonus.js
@@ -18,19 +18,50 @@ const dragon = {
   damage: undefined,
 };
   
-const dragonDamage = () => Math.floor((Math.random() * 35) + 15);
+const dragonDamage = () => Math.floor((Math.random() * dragon.strength) + 15);
 
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
-console.log(dragonDamage());
+const warriorDamage = () => {
+  const dmg = warrior.strength * warrior.weaponDmg;
+  return Math.floor((Math.random() * dmg) + warrior.strength);
+};
 
+const mageDamage = () => {
+  const dmg = mage.intelligence;
+  if (mage.mana > 15) {
+    const dmgDone = Math.floor((Math.random() * dmg) + dmg);
+    mage.mana -= 15;
+    return { Damage: dmgDone, ManaSpent: 15 };
+  };
+
+return 'Não possui mana suficiente';
+};
+
+const gameActions = {
+  // Crie as HOFs neste objeto.
+  wrBattle: (wrdamage) => {
+    wrdamage = warriorDamage();
+    warrior.damage = warrior.damage === undefined ? wrdamage : + wrdamage;
+    dragon.healthPoints -= wrdamage;
+  },
+
+  mageBattle: (mageDmg) => {
+    // mageDmg = mageDamage();
+    mage.damage = mage.damage === undefined ? mageDmg.Damage : + mageDmg.Damage;
+    dragon.healthPoints -= mageDmg.Damage;
+  },
+
+  dragonBattle: (drgDmg) => {
+    // drgDmg = dragonDamage();
+    dragon.damage = dragon.damage === undefined ? drgDmg : + drgDmg;
+    mage.healthPoints -= drgDmg;
+    warrior.healthPoints -= drgDmg;
+  },
+  
+};
+gameActions.dragonBattle(dragonDamage())
+gameActions.mageBattle(mageDamage());
+gameActions.wrBattle(warriorDamage());
 
 const battleMembers = { mage, warrior, dragon };
+
+console.log(battleMembers);
