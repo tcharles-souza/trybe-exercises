a19c81aae47fa5fcadc140a8d5019c04e7e87055
Tcharles Souza <tcharlesvicente@gmail.com>
2023-03-17 13:41:19 -0300

    finish the exercise

--- _front-end/section1/lesson1.1/src/main.js
+++ _front-end/section1/lesson1.1/src/main.js
@@ -1,20 +1,70 @@
 import validator from 'validator';
-import isEmail from 'validator/lib/isEmail';
-import isMobilePhone from 'validator/lib/isMobilePhone'
-import isTaxID from 'validator/lib/isTaxID'
+import './style.css';
 
 const validateBtn = document.querySelector('#validator');
 const validateTxt = document.querySelector('#textToValidate');
+const selectValue = document.querySelector('select');
+const message = document.querySelector('p');
 
+const validateEmail = (email) => {
+  if (validator.isEmail(email)) {
+    message.innerHTML = 'Email validado com sucesso';
+  } else {
+    message.innerHTML = 'Email inválido';
+  }
+};
 
-// validateBtn.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     isEmail(validateTxt.value);
-// });
+const validateCPF = (cpf) => {
+  if (validator.isTaxID(cpf, 'pt-BR')) {
+    message.innerHTML = 'CPF validado com sucesso';
+  } else {
+    message.innerHTML = 'Inválido, o CPF deve conter 11 números';
+  }
+};
 
-// isMobilePhone
+const validateTel = (tel) => {
+  if (validator.isMobilePhone(tel, 'pt-BR')) {
+    message.innerHTML = 'Telefone validado com sucesso';
+  } else {
+    message.innerHTML = 'Inávalido, digite o número corretamente';
+  }
+};
+
+const validateCEP = (cep) => {
+  if (validator.isPostalCode(cep, 'BR')) {
+    message.innerHTML = 'CEP validado com sucesso';
+  } else {
+    message.innerHTML = 'CEP Inválido, digite no formato XXXXX-XX';
+  }
+};
+
+const validateDate = (date) => {
+  if (validator.isDate(date, 'DD/MM/YYYY')) {
+    message.innerHTML = 'Data validada com sucesso';
+  } else {
+    message.innerHTML = 'Data inválida, digite no DD/MM/AAAA';
+  }
+};
 
 validateBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-   console.log(isTaxID(validateTxt.value, 'pt-BR'));
+  event.preventDefault();
+  switch (selectValue.value) {
+  case 'email':
+    validateEmail(validateTxt.value);
+    break;
+  case 'cpf':
+    validateCPF(validateTxt.value);
+    break;
+  case 'telefone':
+    validateTel(validateTxt.value);
+    break;
+  case 'cep':
+    validateCEP(validateTxt.value);
+    break;
+  case 'nascimento':
+    validateDate(validateTxt.value);
+    break;
+  default:
+    message.innerHTML = 'Escolha uma opção';
+  }
 });
